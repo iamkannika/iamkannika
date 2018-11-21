90a38309e1656326c895dad5656017c55d69db11
iamkannika <44707521+iamkannika@users.noreply.github.com>
2018-11-21 11:10:27 +0700

    Update welcome.blade.php
    backgroud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Mook cafe</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -12,10 +12,10 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #FFB6C1;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+                font-weight: 400;
                 height: 100vh;
                 margin: 0;
             }
@@ -95,6 +95,7 @@
                     <a href="">{{$coffee->price}}</a>
                     @endforeach
                 </div>
+
             </div>
         </div>
     </body>
